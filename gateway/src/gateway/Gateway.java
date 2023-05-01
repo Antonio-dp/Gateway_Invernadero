dbc26478f588237bd398a5328d5e36b1b457bed2
Antonio-dp <antonio.delpardo229161@potros.itson.edu.mx>
2023-05-01 14:57:46 -0700

    Update gateway

--- gateway/src/gateway/Gateway.java
+++ gateway/src/gateway/Gateway.java
@@ -4,17 +4,76 @@
  */
 package gateway;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 /**
  *
  * @author tonyd
  */
-public class Gateway {
+public class Gateway implements Runnable{
+    private int puerto;
+    private BufferedReader in;
+    private BufferedWriter out;
+    private Socket sc;
+    private static Gateway gateway;
+    
+    public Gateway(int puerto){
+        this.puerto = puerto;
+    }
+    
+    public static Gateway getInstance(){
+        if(gateway == null){
+            gateway = new Gateway(9000);
+        }
+        return gateway;
+    }
+    
+    public void iniciarListener(){
+        new Thread(gateway).start();
+    }
+    
+    @Override
+    public void run() {
+        final String HOST = "127.0.0.1";
+        try{
+            sc = new Socket(HOST, puerto);
+            in = new BufferedReader(new InputStreamReader(sc.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(sc.getOutputStream()));
+            while(true){
+                String mensaje = in.readLine();
+                if(mensaje == null) break;
+                
+            }
+        } catch(IOException ie){
+            cerrarTodo(sc, in, out);
+        }
+    }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public void enviarMensaje(String mensaje){
+        try{
+                out.write(mensaje);
+                out.newLine();
+                out.flush();
+        } catch(IOException io){
+            io.printStackTrace();
+        }
+        
+    }
+    
+    public void cerrarTodo(Socket socket, BufferedReader in, BufferedWriter out){
+        try{
+            socket.close();
+            in.close();
+            out.close();
+        } catch(IOException io){
+            io.printStackTrace();
+        }
+        
     }
     
 }
