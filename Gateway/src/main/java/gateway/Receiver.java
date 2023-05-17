b3ec29257858121845db8db17b431b4a1f5fff2c
Vastem <vastemxt@gmail.com>
2023-05-17 01:41:18 -0700

    feat: cifrado rsa

--- Gateway/src/main/java/gateway/Receiver.java
+++ Gateway/src/main/java/gateway/Receiver.java
@@ -5,8 +5,6 @@
 package gateway;
 
 import DAOS.SensorDAO;
-import Entidades.Registro;
-import Entidades.Sensor;
 import com.rabbitmq.client.AMQP;
 import java.io.IOException;
 import com.rabbitmq.client.ConnectionFactory;
@@ -16,19 +14,18 @@ import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 import conexiones.ConexionBD;
-import java.io.ByteArrayInputStream;
-import java.io.ObjectInputStream;
-import java.util.Calendar;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.bson.types.ObjectId;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+import javax.crypto.Cipher;
 
 /**
  *
  * @author Vastem
  */
 public class Receiver {
-
+    private static PrivateKey privateKey;
     private final static String QUEUE_NAME = "cola_datos";
     static Gateway gw;
     private static ConexionBD conn = new ConexionBD();
@@ -37,6 +34,12 @@ public class Receiver {
     public static void main(String[] argv) throws Exception {
         sen = new SensorDAO(conn);
         gw = new Gateway("127.0.0.1", 9000);
+
+        // Configurar la clave privada para descifrar
+        String privateKeyString = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQClh2OMregfYA3ZJshz0KvKrFU7putrxu0zJHwyMQMgYB7x/+2a6uSrfwFbSbavgv6/u6J2tDvmysaLqcG6O36wBDS5kaJT5FMIQlmISNIGumGUsqG0+IASBX47C+lbeBghx8jGYsxi8hsn6pW6mPppjWLWfOYkTOUheGZBRpuyWtGXAZXRZzSN5hidNCyvH4Qm9i4Ie9fxR9+lcWMSO+hljUBCPz3MeES0ybB+4Bwz4yPZlv3oRWDZIaduP0bX3N5+ysPYq3FuxUUdquKy0jz0v5UeRDKpCwajvpzzCaRRD+2RtVXW4vwlVX5C4LaS0C69zNvwVXp1L3AJzT69mSRHAgMBAAECggEAO/ncZiK1Enk5S080HlKjjGH42A5ZCsofNAKqRX1gxqBNVh4HN7SYelMgaLVCzkFGkK4p5ZzUf4FFg2FU4megNaKwf/R2vrLiKwHvcuP/xZROuxQmZ24K6Xy4Cij8urm+9K5w3wD5UXirwkDLU7sOMKScBV9n/AsokoIPeuNckrijPvj6kalf0uMcAkthH4wE1BymdixJGAwlLCfxJr02RTqccXM4KXwlMoxRldPo2HPXyibC8a3/RsjHek63271g1udb5QU0Zhb1Io8sCubRorZXIYjeYOp6pjeowUOonc/fLnb5qWNmvfSIPCuvBgg4goE0q5KQ8pHQ04tu/+O3sQKBgQC3VXU3Hhl6Ik7+BnfSPmy+f5o9iuRO1TJS4BG9vRK5bDyrDaaujpHhr4/c9Pt+xn+Jru2g8obA/aURY2d9d2SN0opVPmCGiz/jxuJxRXyPC1jFMZFFH3bQn+JBniRsI7+iFtAUivnhy2+OyvqYZ1eXxcizwBVKSBn9CpEJwdZlbQKBgQDnI0uKE0ZLhsp0KWznxBuJFgS9CTHb/RY/jlClf3Bpcall9qc/QzNlr8LetLg77BPB3InJBYR7+Hb6LxXyXECM/7xb7SRJGUJgF/iP2X/kGdQ9F4qsZwGu/UJ2L/S+ugTXIWRWW7Qle92LwqHMTcMUNj7xTlZZC2V9+o20I5lEAwKBgH8YPldEhZL2394Yq85Tul1h5pKNi/Let7FeZs4rmiRzVaebohbW/WkApXIfX44mm9neLBxspWB7NojUabVAJLRw5bdss8vyEwucH/U4n74mtpaV40iRJHRCsr6cnFFfgwUiXYQwSETxHFhYfNtUoRO1aIq8OcYyL9oNDjk+aJwNAoGBAIFM91cYjTx2/Q6alffZduXrUV9Go4PDQwzu2iKa9hGmqfMGVm1HdJswBb18L4wl9q9+Zf30fjazuise6BIalWnLLl3mfWP/I2iKQFyIecjqwhYi80qanRB9UQ64qRuBCHAol+7Pgilt73Pdv5GA9t55siBfiGJrw60D2v2o5HZtAoGAMCHSr4n4dZebYu2Gn6jnSYhPsW8oiuuS606i6/gzqEoPgjujG1KvMN6L9YIFEPwWEabnCgoa5tKzUWWHMVGp7OIgJ1l7mFdpU8k5fr0DCPqnyp2Mq/vLyG6eCcORn8ogbs50hMOd5PASMVgXRUVwBpCfilGJ4TlX/u9eFIl6vY0="; // Reemplazar con tu clave privada generada
+        byte[] privateKeyBytes = Base64.getDecoder().decode(privateKeyString);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        privateKey = keyFactory.generatePrivate(new PKCS8EncodedKeySpec(privateKeyBytes));
 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
@@ -50,30 +53,10 @@ public class Receiver {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 try {
-                    String message = new String(body, "UTF-8");
-//                    String[] values = message.split(",");
-//                    float temperatura = Float.parseFloat(values[0]);
-//                    float humedad = Float.parseFloat(values[1]);
-//                    long fechaMillis = Long.parseLong(values[2]);
-//                    String sensorId = values[3];
-//
-//                    ObjectId id = new ObjectId(sensorId);
-//                    Sensor sensor = sen.consultarSensor(id);
-//
-//                     Crear el objeto Registro
-//                    Registro registro = new Registro();
-//                    registro.setTemperatura(temperatura);
-//                    registro.setHumedad(humedad);
-//                    Calendar fecha = Calendar.getInstance();
-//                    fecha.setTimeInMillis(fechaMillis);
-//                    registro.setFecha(fecha);
-//                    registro.setSensor(sensor);
-                    
-                    
-                    //channel.basicAck(envelope.getDeliveryTag(), false);
-                    
-                    System.out.println("Mensaje recibido: " + message);
-                    send(message);
+                    String decryptedData = decryptData(body);
+
+                    System.out.println("Mensaje recibido: " + decryptedData);
+                    send(decryptedData);
                 } catch (Exception ex) {
                     //Logger.getLogger(Receiver.class.getName()).log(Level.SEVERE, null, ex);
                 }
@@ -87,4 +70,10 @@ public class Receiver {
         channel.basicConsume(QUEUE_NAME, true, consumer);
     }
 
+    private static String decryptData(byte[] encryptedData) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedBytes = cipher.doFinal(encryptedData);
+        return new String(decryptedBytes);
+    }
 }
